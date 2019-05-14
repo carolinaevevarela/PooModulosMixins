dc703c9e3830b8c9271712eaa9062f6d35acf97d
Carolina <carolinaevevarela@gmail.com>
2019-05-13 21:18:43 -0400

    4to commit

--- car.rb
+++ car.rb
@@ -1,2 +1,20 @@
+require_relative 'vehicle.rb'
 class Car < Vehicle
+  @@tries = 0
+  def initialize(model, year)
+    super
+    @@tries += 1
+  end
+  def self.counter
+    @@tries
+  end
+
+  def engine_start
+    super
+    puts "El motor del #{@model} del año #{@year} se ha encendido"
+  end
 end
+
+10.times { Car.new("Camaro", 1970).engine_start}
+
+puts Car.counter
