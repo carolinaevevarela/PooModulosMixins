4035533ca4974fbd5d2478f8bcd68f6db8195c4f
Carolina <carolinaevevarela@gmail.com>
2019-05-15 22:07:55 -0400

    Fuera de clase 2

--- student.rb
+++ student.rb
@@ -1,6 +1,6 @@
 module Test
   def result
-    (@nota1 + @nota2) * 2
+    result = (@nota1 + @nota2) * 2
     if result > 4
       puts "Estudiante aprobado"
     else
@@ -10,23 +10,28 @@ module Test
 end
 
 module Attendance
-  def student_quantity
-    puts Student.quantity
+  def self.student_quantity
+    Student.quantity
   end
 end
 
 class Student
+  include Test
+  extend Attendance
   attr_accessor :nombre, :nota1, :nota2
   @@quantity = 0
+
   def initialize(nombre, nota1 = 4, nota2 = 4)
     @nombre = nombre
     @nota1 = nota1
     @nota2 = nota2
     @@quantity += 1
   end
-  def quantity
+  def self.quantity
     @@quantity
   end
 end
 
-10.times do Student.new
+students = 10.times do Student.new('Kevin').result end
+puts "El total de estudiantes es #{students}"
+puts "El número de alumnos asistentes es #{Attendance.student_quantity}"
